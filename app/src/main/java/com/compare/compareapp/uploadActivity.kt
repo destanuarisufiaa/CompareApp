30195dd3abcfaeb9d471f30968a5085e1d6d4179
destanuarisufiaa <destanuarisufia25@gmail.com>
2023-01-18 17:42:58 +0700

    berhasil menambahkan fab dan masuk ke activity upload data

---
+++ app/src/main/java/com/compare/compareapp/uploadActivity.kt
@@ -0,0 +1,11 @@
+package com.compare.compareapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class uploadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload)
+    }
+}
